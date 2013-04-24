7ee62552022b0eb5754015786dfb467ed48f8aac
nineoh <n_lanfranchi@hotmail.com>
2013-04-24 18:31:52 +0200

    merge with Jann's project

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,130 @@
-$(function() {
+/*
+ * Jann's section
+ */
+var UniversalTuringMachine = function () {
+
+	var me = this;
+	me.headPosition = 0;
+	me.state = "0";
+	me.tape = [];
+	me.stepCount = 0;
+	me.successCallback = null;
+	me.errorCallback = null;
+	me.postStepCallback = null;
+	me.startCallback = null;
+	me.stepInterval = 100;
+
+	this.compare = function (a, b) {
+		return a.toLowerCase() === b.toLowerCase();
+	}
+
+	this.moveLeft = function () {
+		if (me.headPosition === 0) {
+			me.tape.reverse().push('_');
+			me.tape.reverse();
+			alert('not good');
+		} else {
+			me.headPosition--;
+		}
+	}
+
+	this.moveRight = function () {
+		me.headPosition++;
+
+		if (!me.tape[me.headPosition])
+			me.tape[me.headPosition] = "_";
+	}
+
+	this.go = function (input, programm) {
+		me.tape = input.split('');
+		if (me.startCallback) me.startCallback(me);
+
+		(function loop() {
+			setTimeout(function () {
+
+				var ruleRan = false;
+
+				for (var i = 0; i < programm.length; i++) {
+	                // Match actual me.state and actual value on me.tape
+	                if (me.compare(programm[i][1], me.tape[me.headPosition])
+	                && me.compare(programm[i][0], me.state)) {
+
+	                	me.tape[me.headPosition] = programm[i][2];
+
+		                if (me.compare(programm[i][3], "R"))
+		                	me.moveRight();
+		                else if (me.compare(programm[i][3], "L"))
+		                	me.moveLeft();
+
+		                me.state = programm[i][4];
+
+		                ruleRan = true;
+		                me.stepCount++;
+		                if (me.postStepCallback) me.postStepCallback(me);
+		                break;
+	            	}
+	        	}
+
+		        if (ruleRan) {
+		        	loop();
+		        } else {
+		        	if (me.compare(me.state, "END")) {
+		        		if (me.successCallback) me.successCallback();
+		        	} else {
+		        		if (me.errorCallback) me.errorCallback();
+		        	}
+		        }
+		    }, me.stepInterval)
+		})();
+	}
+}
+
+var Tools = function (turing) {
+	var me = this;
+	me.tm = turing;
+
+	this.formatTape = function () {
+		var result = "";
+
+		for (var i = 0; i < me.tm.tape.length; i++) {
+			var item = me.tm.tape[i];
+			var lspacer = "";
+			var rspacer = "";
+			var itemSpacer = "&nbsp;";
+
+			if (i === me.tm.headPosition) {
+				lspacer = "[";
+				rspacer = "]";
+			} else {
+				lspacer = "&nbsp;";
+				rspacer = "&nbsp;";
+			}
+
+			result = (result + lspacer + item + rspacer + itemSpacer);
+		}
+		return result;
+	}
+
+	this.logTape = function (text) {
+		$('#outputField').append('<div>' + me.formatTape() + '</div>');
+	}
+}
+
+Tools.init = function () {
+	$('#outputField').empty();
+	$('SPAN#status').text('idle');
+	$('SPAN#stepCount').text('0');
+	$('#tape').text('');
+	$('#programm').text('');
+}
+
+var addition = '[["0", "0", "_", "R", "1"],["0", "1", "_", "R", "2"],["1", "0", "0", "R", "1"],["1", "1", "0", "R", "2"],["2", "0", "0", "R", "2"],["2", "1", "_", "R", "END"]]';
+
+
+/*
+ * Nino's section
+ */
+
 /*
  * Initialize tape with the two digits which were entered.
  */
@@ -14,7 +140,7 @@ $(function() {
  	var offset = 15 - tapeLength;
  	var fieldCounter = 0;
 
- 	$('ul#tape li.active ~ li').remove();
+ 	$('ul#fancyTape li.active ~ li').remove();
 
 	// Create tape cells for the input values
 	var liTag = '';
@@ -22,13 +148,13 @@ $(function() {
 		liTag = i < 15 ? '<li>' : '<li class="hidden">';
 
 		if (i == (firstVal))
-			$('ul#tape').append(liTag + '&nbsp;</li>');
+			$('ul#fancyTape').append(liTag + '&nbsp;</li>');
 		else
-			$('ul#tape').append(liTag + '&bull;</li>');
+			$('ul#fancyTape').append(liTag + '&bull;</li>');
 	}
 	// fill up the rest of the tape with empty cells
 	for (var i = 0; i < offset; i++) {
-		$('ul#tape').append('<li>&nbsp;</li>');
+		$('ul#fancyTape').append('<li>&nbsp;</li>');
 	}
 
 };
@@ -75,12 +201,67 @@ $(function() {
  }
 
 
-// Initialzie event handlers
- $('#txtFirstVal').change(resetErrorState);
- $('#txtSecondVal').change(resetErrorState);
- $('#btnInitialize').click(initializeUi);
+$(function () {
+	/*
+	 * Nino's section
+	 */
 
- $('#btnRun').click(runMachine);
- $('#btnStep').click(doStep);
+	 // Initialzie event handlers
+	$('#txtFirstVal').change(resetErrorState);
+	$('#txtSecondVal').change(resetErrorState);
+	$('#btnInitialize').click(initializeUi);
+	$('#btnRun').click(runMachine);
+	$('#btnStep').click(doStep);
 
+
+	/*
+	 * Jann's section
+	 */
+	$('#reset').click(function () {
+		Tools.init();
+	}).click();
+
+	$('#loadadd').click(function () {
+		$('#programm').text(addition);
+	});
+
+	$('#stepInterval').change(function (e) {
+		$(document).trigger('speedchange', parseInt($(this).val()));
+	}).change();
+
+	$('#start').click(function () {
+		var turing = new UniversalTuringMachine();
+		var tools = new Tools(turing);
+
+		var tape = $('#tape').val();
+		var simulator = eval($('#programm').val());
+
+		$(document).off('speedchange');
+		$(document).on('speedchange', function (e, speed) {
+			turing.stepInterval = speed;
+			alert(speed);
+		});
+
+		turing.startCallback = function () {
+			tools.logTape();
+			$('SPAN#status').text('running');
+		}
+
+		turing.successCallback = function () {
+			$('SPAN#status').text('success');
+		};
+
+		turing.errorCallback = function () {
+			$('SPAN#status').text('error!');
+		};
+
+		turing.postStepCallback = function (me) {
+			tools.logTape();
+			$('SPAN#stepCount').text(me.stepCount);
+			$('#tape').text(me.tape.join(''));
+		};
+
+        // Run
+        turing.go(tape, simulator);
+    });
 });
